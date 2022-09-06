1eb1a4b66dc21a5bb86b906a9d41b0f04c42dfaf
Robert Brockmann <bbrockmann1@gmail.com>
2022-09-05 20:40:33 -0600

    commit tree b08280067a636eb3b528d4a548ae0eeee4b038d6

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your code here!
+const testVar = "hi"
+
+/* Enter the code to remove the node element here */
+document.querySelector("main#main").remove();
+
+/* Create your new element here and assign it to newHeader */
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Flatiron School is the champion!";
+newHeader.className = "victory";
+
+
+
+
+
+
+const element = document.createElement('div');
+document.body.append(element);
+const ul = document.createElement("ul");
+
+for (let i = 0; i < 3; i++) {
+    const li = document.createElement("li");
+    li.textContent = (i + 1).toString();
+    ul.append(li);
+  }
+
+element.append(ul);
